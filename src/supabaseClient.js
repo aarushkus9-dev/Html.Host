d11cd79ec7faf8f1346a49c7ce9672bbbcae063e
AarushKus9 <aarushkus9business@gmail.com>
2026-02-04 17:00:29 -0500

    Build output (dist)

--- src/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://llqivnpqeiqplrrdttxt.supabase.co'
-// Using the publishable key you provided
-const supabaseKey = 'sb_publishable__43AjhHmQ-yMMCFfPMM1Mg_sL2iOCB2'
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
